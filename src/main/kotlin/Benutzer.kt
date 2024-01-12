7ef31fb779c7e4c22726ed8ef08097e1481ae6a6
Nikola Djukic <nikola.djukic@bluewin.ch>
2024-01-12 19:03:55 +0100

    12-01-24 Erstellen des Projekt v02

---
+++ src/main/kotlin/Benutzer.kt
@@ -0,0 +1,16 @@
+class Benutzer {
+
+    userid:   int
+    passwort: String
+    vorname:  String
+    Nachname: String
+    e-mail:   String
+    Funktion: String
+
+
+    private val userid
+    private var passwort
+    private var vorname
+
+}
+
