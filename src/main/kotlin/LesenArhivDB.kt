46da0bdf9b4bdd760e25be9598145c89c8944b5d
Alexandru Luca <alexandru.lucaa@gmail.com>
2024-01-31 21:32:12 +0100

    Lese Arhiv

--- src/main/kotlin/LesenArhivDB.kt
+++ src/main/kotlin/LesenArhivDB.kt
@@ -1,11 +1,10 @@
 import java.sql.DriverManager
-import java.time.LocalDate
-import java.time.LocalDateTime
 import java.text.SimpleDateFormat
+import java.time.DayOfWeek
 import java.time.Duration
+import java.time.LocalDate
+import java.time.temporal.ChronoUnit
 import java.util.Date
-import kotlin.time.toKotlinDuration
-import kotlin.time.ExperimentalTime
 
 fun lesenArchivDB(userId: Int) : String {
     // val currentDateTime = LocalDateTime.now()
@@ -18,115 +17,82 @@ fun lesenArchivDB(userId: Int) : String {
     val USER =     "UserTest"
     val PASSWORD = "admin"
 
-    // Verbindung zur DB herstellen4
+    // Verbindung zur DB herstellen
     val connection = DriverManager.getConnection(URL, USER, PASSWORD)
 
     // Statement erzeugen
     val statement = connection.createStatement()
 
-    print("Geben Sie das Startdatum im Format (yyyy-MM-dd) ein: ")
-
-    // Datum von der Konsole lesen
-    val inputDateStr1 = readLine()
-
-    // Datum in ein Date-Objekt umwandeln (falls möglich)
-    val dateFormat1 = SimpleDateFormat("yyyy-MM-dd")
-
-    try {
-        val inputDate1: Date? = dateFormat1.parse(inputDateStr1)
-
-        if (inputDate1 != null) {
-            // Datum ohne Uhrzeit ausgeben
-            val formattedDate1 = dateFormat1.format(inputDate1)
-            println("Eingegebenes Datum: $formattedDate1")
-        } else {
-            println("Ungültiges Datumsformat.")
-        }
-    } catch (e: Exception) {
-        println("Fehler beim Parsen des Datums: ${e.message}")
-    }
-
-    print("Geben Sie das Enddatum im Format (yyyy-MM-dd) ein: ")
-
-    // Datum von der Konsole lesen
-    val inputDateStr2 = readLine()
-
-    // Datum in ein Date-Objekt umwandeln (falls möglich)
-    val dateFormat2 = SimpleDateFormat("yyyy-MM-dd")
-
-    try {
-        val inputDate2: Date? = dateFormat1.parse(inputDateStr1)
-
-        if (inputDate2 != null) {
-            // Datum ohne Uhrzeit ausgeben
-            val formattedDate2 = dateFormat2.format(inputDate2)
-            println("Eingegebenes Datum: $formattedDate2")
-        } else {
-            println("Ungültiges Datumsformat.")
-        }
-    } catch (e: Exception) {
-        println("Fehler beim Parsen des Datums: ${e.message}")
-    }
-
-    print("Geben Sie eine Startuhrzeit im Format (HH:mm:ss) ein: ")
-
-    // Uhrzeit von der Konsole lesen
-    val inputTimeStr1 = readLine()
-
-    // Uhrzeit in ein Date-Objekt umwandeln (falls möglich)
-    val timeFormat1 = SimpleDateFormat("HH:mm:ss")
-
-    try {
-        val inputTime1: Date? = timeFormat1.parse(inputTimeStr1)
-
-        if (inputTime1 != null) {
-            // Uhrzeit ohne Datum ausgeben
-            val formattedTime1 = timeFormat1.format(inputTime1)
-            println("Eingegebene Enduhrzeit: $formattedTime1")
-        } else {
-            println("Ungültiges Uhrzeitformat.")
-        }
-    } catch (e: Exception) {
-        println("Fehler beim Parsen der Uhrzeit: ${e.message}")
-    }
-
-    print("Geben Sie eine Startuhrzeit im Format (HH:mm:ss) ein: ")
-
-    // Uhrzeit von der Konsole lesen
-    val inputTimeStr2 = readLine()
-
-    // Uhrzeit in ein Date-Objekt umwandeln (falls möglich)
-    val timeFormat2 = SimpleDateFormat("HH:mm:ss")
-
-    val inputTime2: Date? = timeFormat2.parse(inputTimeStr2)
-
-    if (inputTime2 != null) {
-        // Uhrzeit ohne Datum ausgeben
-        val formattedTime2 = timeFormat2.format(inputTime2)
-            println("Eingegebene Enduhrzeit: $formattedTime2")
+    println("Geben Sie ein Startdatum im Format (yyyy-MM-dd) ein")
+    // Startdatum von der Konsole lesen
+    val startDatumStr = readlnOrNull()
+    // Prüfen/Validierung des Datumsformats
+    val startDatumFormat = SimpleDateFormat("yyyy-MM-dd")
+    // Startdatum in ein Date-Objekt umwandeln (falls möglich)
+    val startDatum: Date? = startDatumFormat.parse(startDatumStr)
+    if (startDatum != null) {
+        // Startdatum ohne Zeit ausgeben
+        val formattedEndDatum = startDatumFormat.format(startDatum)
+        println("Eingegebene Startdatum: $formattedEndDatum")
     } else {
-            println("Ungültiges Uhrzeitformat.")
+        println("Ungültiges Datumsformat.")
     }
 
+    println("Geben Sie ein Enddatum im Format (yyyy-MM-dd) ein")
+    // Enddatum von der Konsole lesen
+    val endDatumStr = readlnOrNull()
+    // Prüfen/Validierung des Datumsformats
+    val endDatumFormat = SimpleDateFormat("yyyy-MM-dd")
+    // Enddatum in ein Date-Objekt umwandeln (falls möglich)
+    val endDatum: Date? = endDatumFormat.parse(endDatumStr)
+    if (endDatum != null) {
+        // Startdatum ohne Zeit ausgeben
+        val formattedEndDatum = endDatumFormat.format(endDatum)
+        println("Eingegebene Enddatum: $formattedEndDatum")
+    } else {
+        println("Ungültiges Datumsformat.")
+    }
+    val endDatumParce = LocalDate.parse(endDatumStr)
+    val startDatumParce = LocalDate.parse(startDatumStr)
+
+
+    var anzahlArbeitstage = 0
+    if (startDatum != null && endDatum != null) {
+        anzahlArbeitstage = berechneArbeitstage(startDatumParce, endDatumParce)
+//        println("Anzahl der Arbeitstage zwischen $startDatum und $endDatum: $anzahlArbeitstage")
+    } else {
+        println("Ungültiges Datumsformat.")
+    }
 
 
     // SQL erstellen um Zeilen aus DB zu laden
     val sql = "SELECT b.userid, z.datum, z.startzeit, z.endzeit, b.vorname " +
             "FROM Zeiterfassung z, benutzer b " +
-            "WHERE z.datum BETWEEN '$inputDateStr1' AND '$inputDateStr2' " +
-            "AND z.startzeit BETWEEN '$inputTimeStr1' AND  '$inputTimeStr2' " +
+            "WHERE z.datum BETWEEN '$startDatumStr' AND '$endDatumStr' " +
+//            "AND z.startzeit BETWEEN '$startTimeStr' AND  '$endTimeStr' " +
             "AND b.userid = '$userId'"
 
-//    val arbeitsZeit2 = Duration.between(inputDateStr1,inputDateStr2)
-//    var pausenZeit2  = Duration.between(pausenzeitBeginn,pausenzeitEnde)
+
 
     // SQL ausfuehren
     val data = statement.executeQuery(sql)
 
     var totalArbeitszeit: Duration = Duration.ZERO
+    val sollStundenAmTag: Duration = Duration.ofHours(8)
+    val sollStundenAmTagAnzeige = 8
+    var formatiertetotalArbeitszeit = ""
 
+    //var formatierteSollStundenAmTag = formatiereZeit(sollStundenAmTag)
+    val sollStundenAmTagInt = sollStundenAmTag.seconds.toInt()
+    val totalSollStunden = sollStundenAmTagInt  * anzahlArbeitstage /3600
+
+    var minusstunden = Duration.ZERO
+    var ueberstunden = Duration.ZERO
+    var minusDauer = Duration.ZERO
+    var ueberstundenDauer = Duration.ZERO
     // Zeilen ausgeben
     while (data.next()) {
+
 
         val datum = data.getDate("datum")
         val startzeit = data.getTime("startzeit")
@@ -134,27 +100,58 @@ fun lesenArchivDB(userId: Int) : String {
         val vorname = data.getString("vorname")
 
 
-
         val startZeit = startzeit.toLocalTime()
         val endZeit = endzeit.toLocalTime()
 
-        val arbeitZeit = Duration.between(startZeit, endZeit)
-        val minimumarbeitzeit : Duration = Duration.ofSeconds(1)
 
-        if (arbeitZeit >= minimumarbeitzeit){
+
+        val arbeitZeit = Duration.between(startZeit, endZeit)
+//      val arbeitTagenTotal = Duration.between()
+
+
+
+        val minimumArbeitszeit: Duration = Duration.ofSeconds(1)
+
+        val differenzSollStunden: Duration = if (arbeitZeit < sollStundenAmTag) {
+            // Berechnung der Minusstunden
+            minusDauer = sollStundenAmTag.minus(arbeitZeit)
+            minusstunden = minusstunden.plus(minusDauer.negated())
+            Duration.ZERO
+        } else {
+            // Berechnung der Überstunden
+            ueberstundenDauer = arbeitZeit.minus(sollStundenAmTag)
+            ueberstunden = ueberstunden.plus(ueberstundenDauer)
+            Duration.ZERO
+        }
+
+
+        val formatiertearbeitZeit = formatiereZeit(arbeitZeit)
+        val formatiertedifferenzSollStundenminus = formatiereZeit(minusDauer)
+        val formatiertedifferenzSollStundenplus = formatiereZeit(ueberstundenDauer)
+        if (arbeitZeit >= minimumArbeitszeit){
             totalArbeitszeit += arbeitZeit
+            formatiertetotalArbeitszeit  = formatiereZeit(totalArbeitszeit)
         }
         else{continue}
 
 
 
         println("UserID: $userId | Datum: $datum | Startzeit: $startzeit | Endzeit: $endzeit | Vorname: $vorname")
-        println("Arbeitszeit: $arbeitZeit")
-        val total = kotlin.time.Duration
+        println("Arbeitszeit: $formatiertearbeitZeit, Sollstunden: $sollStundenAmTagAnzeige h , Differenz - $formatiertedifferenzSollStundenminus  + $formatiertedifferenzSollStundenplus")
 
     }
-    println("Gesamtarbeitszeit: $totalArbeitszeit")
+    println("Gesamtarbeitszeit: $formatiertetotalArbeitszeit ")
+    println("Anzahl Arbeitstagen $anzahlArbeitstage, Gesamtsollstunden: $totalSollStunden, $minusstunden $ueberstunden")
     userIdString = userId.toString()
 
     return "$userIdString $passwort $vorname $nachname $email $funktion $admin"
+}
+
+fun berechneArbeitstage(startDatum: LocalDate, endDatum: LocalDate): Int {
+    // Arbeitstage zählen, ohne Wochenenden
+    val anzahlTage = ChronoUnit.DAYS.between(startDatum, endDatum) + 1
+    val anzahlTageInt = anzahlTage.toInt()
+    val wochenenden = (0..anzahlTage).map { startDatum.plusDays(it) }
+        .count { it.dayOfWeek == DayOfWeek.SATURDAY || it.dayOfWeek == DayOfWeek.SUNDAY }
+    return anzahlTageInt  - wochenenden
 }
